6d60d9ec07dac3f5aec17734571c2e3c9808042a
Sampada Bhagat <sampada.bhagat@veritas.com>
2018-10-11 16:53:41 +0530

    setting button styles

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -65,3 +65,17 @@ var app = {
 $(document).ready(() => {
     console.log('ready');
 });
+$(document).on("pagecreate", "#landing-page", function () {
+
+    /* change event handler */
+    function flipChanged(e) {
+        var id = this.id,
+            value = this.value;
+        console.log(id + " has been changed! " + value);
+        app.isOIBMode = value;
+        OIB.initialize('hideThis', 'OIB-Div');
+    }
+
+    /* add listener - this will be removed once other buttons are clicked */
+    $("#flip-mini").on("change", flipChanged);
+});
